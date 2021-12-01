e1f648470638081b7b1e6b37a1ea180878e7954a
ldkhang4869 <ldkhang4869@gmail.com>
2021-12-01 15:31:34 +0700

    Change input shape

--- keras_ocr/detection.py
+++ keras_ocr/detection.py
@@ -695,6 +695,7 @@ class Detector:
             )
         else:
             weights_path = None
+        del self.model
         self.model = build_keras_model(
             weights_path=weights_path, backbone_name=backbone_name, input_shape=input_shape
         )
