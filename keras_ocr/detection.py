aa48ea25d936c6a1fef1d4df265397e876843eed
ldkhang4869 <ldkhang4869@gmail.com>
2021-12-01 13:55:06 +0700

    Fix eager mode

--- keras_ocr/detection.py
+++ keras_ocr/detection.py
@@ -696,7 +696,7 @@ class Detector:
         self.model = build_keras_model(
             weights_path=weights_path, backbone_name=backbone_name, inputs=inputs
         )
-        self.model.compile(loss="mse", optimizer=optimizer)
+        self.model.compile(loss="mse", run_eagerly = True, optimizer=optimizer)
 
     def get_batch_generator(
         self,
